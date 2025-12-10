83d795bb5c1f7a7faad9a8028f0f537576d06d4f
10-OREO <cristolepetenorio@gmail.com>
2025-12-09 20:58:43 -0800

    Scene Manager Fix

--- Assets/Scripts/Finish Line.cs
+++ Assets/Scripts/Finish Line.cs
@@ -23,6 +23,7 @@ public class FinishLine : MonoBehaviour
                 mazeController.enabled = false;
 
             // Load next scene
+            SceneManager.LoadScene("HoomanScene");
         }
     }
 }
