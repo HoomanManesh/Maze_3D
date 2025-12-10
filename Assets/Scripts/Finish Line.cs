81b1e5c41b79de53c6be5dca042e5a304aa8098e
10-OREO <cristolepetenorio@gmail.com>
2025-12-09 20:40:12 -0800

    Finish Line Detector

--- Assets/Scripts/Finish Line.cs
+++ Assets/Scripts/Finish Line.cs
@@ -1,24 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-   [Header("Name of the scene to load")]
-    public string nextSceneName;
+    public BoardController mazeController;  
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the thing that entered the trigger is the ball (tagged Player)
         if (other.CompareTag("Player"))
         {
-            // (Optional) stop the ball movement before changing scenes
+            // Freeze ball
             Rigidbody rb = other.attachedRigidbody;
             if (rb != null)
             {
                 rb.linearVelocity = Vector3.zero;
                 rb.angularVelocity = Vector3.zero;
+                rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
             }
 
-            // Load the next scene
+            // Freeze maze movement
+            if (mazeController != null)
+                mazeController.enabled = false;
+
+            // Load next scene
         }
     }
 }
